a21878df27d8e87993927a2a0aa419b08a35ff7d
dewshan chathuranga <85221292+dechathuranga@users.noreply.github.com>
2021-09-27 22:20:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,5 +8,9 @@ export default function Page () {
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
     </Layout>
+
+<script type="application/javascript">
+  (function(b,o,n,g,s,r,c){if(b[s])return;b[s]={};b[s].scriptToken="XzIxNzExNDA2OQ";b[s].callsQueue=[];b[s].api=function(){b[s].callsQueue.push(arguments);};r=o.createElement(n);c=o.getElementsByTagName(n)[0];r.async=1;r.src=g;r.id=s+n;c.parentNode.insertBefore(r,c);})(window,document,"script","https://cdn.oribi.io/XzIxNzExNDA2OQ/oribi.js","ORIBI");
+</script>
   )
 }
